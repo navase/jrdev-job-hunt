0255a8619ee3964a5ec7cff40fac15923c736e82
irisbune <irisbune@gmail.com>
2016-03-04 16:02:09 +0100

    added validation messages

--- app/models/company_profile.rb
+++ app/models/company_profile.rb
@@ -1,8 +1,11 @@
 class CompanyProfile < ActiveRecord::Base
   belongs_to :company
-  validates_presence_of :name, message: "Name cannot be empty", :description, :size, :salary_indication, :jr_sr_ratio, presence: true
-  validates_inclusion_of :size, in: %w(0-10 10-50 50-100 >100), allow_blank: false
-  validates_inclusion_of :salary_indication, in: %w(<2000 2000-2200 2200-2400 2400-2600 2600-2800 2800-3000), allow_blank: false
+  validates_presence_of :name, message: "Name cannot be empty."
+  validates_presence_of :description, message: "Description cannot be empty."
+  validates :size, :salary_indication, presence: true
+  validates_presence_of :jr_sr_ratio, message: "Jr/Sr ratio cannot be empty."
+  validates_inclusion_of :size, in: %w(0-10 10-50 50-100 >100), allow_blank: false, message: "Invalid input for Size."
+  validates_inclusion_of :salary_indication, in: %w(<2000 2000-2200 2200-2400 2400-2600 2600-2800 2800-3000), allow_blank: false, message: "Invalid input for salary indication."
 
 
 

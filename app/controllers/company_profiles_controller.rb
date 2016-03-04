31738111bd65a26c63749959e85075ee096b0d96
irisbune <irisbune@gmail.com>
2016-03-04 16:34:03 +0100

    added current_company.id to create method

--- app/controllers/company_profiles_controller.rb
+++ app/controllers/company_profiles_controller.rb
@@ -12,11 +12,12 @@ before_action :authenticate_company!, only: [:create, :update, :destroy]
   def new
     # new profile has to be linked to current singed in company
     @profile = CompanyProfile.new
-    @profile.company_id = current_company
   end
 
   def create
-    if @profile = CompanyProfile.create(profile_params)
+    @profile = CompanyProfile.new(profile_params)
+    @profile.company_id = current_company.id
+    if @profile.save
       redirect_to @profile
     else
       render :new

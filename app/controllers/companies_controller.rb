ca1ad0758049717d9100e56e1a3fc20345c978ba
irisbune <irisbune@gmail.com>
2016-03-04 18:26:36 +0100

    added json for company show

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,8 +1,16 @@
 class CompaniesController < ApplicationController
 
   def show
-    @company = Company.find(params[:id])
-    @company_profiles = @company.company_profiles
+    company = Company.find(params[:id])
+    company_profiles = company.company_profiles
+
+    render json:{
+      meta: {
+        company_profiles_count: company_profiles.count,
+        page: 0
+      },
+      company: company.as_json({include: :company_profiles})
+    }
   end
 
 end

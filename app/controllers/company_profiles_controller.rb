ec95c1489de9d63cfdc9f8e33a09c6a313908563
irisbune <irisbune@gmail.com>
2016-03-04 14:29:47 +0100

    added profile update form to controller and view. Added validation to model

--- app/controllers/company_profiles_controller.rb
+++ app/controllers/company_profiles_controller.rb
@@ -9,10 +9,22 @@ before_action :authenticate_company!, only: [:create, :update, :destroy]
     @profile = CompanyProfile.find(params[:id])
   end
 
+  def new
+    @profile = CompanyProfile.new
+  end
+
+  def create
+    if @profile = CompanyProfile.create(profile_params)
+      redirect_to @profile
+    else
+      render :new
+    end
+  end
+
 
   private
 
-  def company_params
-    params.require(:company).permit(:name, :description, :size, :salary_indication, :jr_sr_ratio, :company_id )
+  def profile_params
+    params.require(:company_profile).permit(:name, :description, :size, :salary_indication, :jr_sr_ratio, :company_id )
   end
 end
--- app/models/company_profile.rb
+++ app/models/company_profile.rb
@@ -1,3 +1,5 @@
 class CompanyProfile < ActiveRecord::Base
   belongs_to :company
+  validates :name, :description, :size, :salary_indication, :jr_sr_ratio, presence: true
+
 end

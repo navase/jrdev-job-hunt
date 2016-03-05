802d1d5520dd7e0240dffb3a0ebd11cf02165c82
irisbune <irisbune@gmail.com>
2016-03-05 16:16:06 +0100

    removed regex

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 class JrdevjobhuntBack::Application
   config.middleware.insert_before 0,  "Rack::Cors", debug: Rails.env.development?, logger: (-> { Rails.logger }) do
     allow do
-      origins 'localhost:3000', '127.0.0.1:3000', 'developme.bitballoon.com/'
-      
+      origins 'localhost:3000', '127.0.0.1:3000', 'developme.bitballoon.com'
+
 
       resource '/cors',
         headers: :any,
